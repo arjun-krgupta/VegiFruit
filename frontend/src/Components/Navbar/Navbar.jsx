afd049f92a1b95847861bcecae1e76fa6a6ad3a6
Arjun kumar <akarjun1050@gmail.com>
2024-12-06 16:38:00 +0530

    update navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -34,23 +34,29 @@ function Navbar() {
             <Link to='/'><h1 className="font-bold">VegiFruit</h1></Link>
             <i className="fa-solid fa-bars togle"></i>
           </div>
-         
-            {
-              auth ? 
-              <div className="menu">
-              <Link to='/'>HOME</Link>
-              <Link to='fruit'>Fruits</Link>
-              <Link to='vegetable'>Vegetables</Link>
-              <Link to='/contact'>Contact</Link>
-              <Link to='/signup' onClick={logout}>Logout</Link> 
-              </div>
-              :
-              <div className="login-cart">
+          {
+            auth ? 
+
+          <div className="bg-[rgb(80, 141, 78)] flex justify-end md:justify-between items-center w-full h-14">
+              <div className="hidden md:flex md:justify-center text-white md:gap-8 lg:gap-12 md:mx-auto">
+                <Link to='/'>Home</Link>
+                <Link to='fruit'>Fruits</Link>
+                <Link to='vegetable'>Vegetables</Link>
+                <Link to='/contact'>Contact</Link>
+            </div>
+            <div className=" text-white pe-8 md:pe-10 lg:pe-16">
+            <Link to='/signup' onClick={logout}>Logout</Link>
+            </div>
+          </div>
+            :
+            <div className="bg-[rgb(80, 141, 78)] flex justify-end items-center w-full h-14">
+              <div className="text-white flex gap-5 pe-7">
               <Link to='/signup'>SignUp</Link>
               <Link to='/login'>Login</Link>
               </div>
-            }
-           
+            </div>
+
+          }
           <div className="login-cart">
             <Link to='/cart'><i className="fa-solid fa-bag-shopping cart"></i></Link>
             <span className="cart-count">0</span>
